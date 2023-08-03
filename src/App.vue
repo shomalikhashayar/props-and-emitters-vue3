3f47f5b84d4b992c7534494832a65b78e386f310
Khashayar Shomali <khashi792017@gmail.com>
2023-08-03 23:44:14 +0330

    How to use props and emits in vue3 with script setup syntax.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,101 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <card :userData="userData" @readMore="readMore" />
+  <div id="myModal" class="modal" v-if="showModal">
+    <!-- Modal content -->
+    <div class="modal-content">
+      <span class="close" @click="showModal = false">&times;</span>
+      <p>{{ userData[0].description }}</p>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from "vue";
+import Card from "./components/Card.vue";
+const showModal = ref(false);
+const userData = ref([
+  {
+    id: "001",
+    name: "Khashayar Shomali",
+    jobTitle: "Front-end Developer(Vue.js)",
+    description:
+      "Khashayar Shomali is a talented and innovative Front-End Developer, specializing in Vue.js, who has made significant contributions to the world of web development. With a passion for crafting seamless user experiences and a keen eye for design, Khashayar has earned a reputation for building cutting-edge and visually stunning web applications.",
+  },
+  {
+    id: "002",
+    name: "Yousef Aria",
+    jobTitle: "Front-end Developer(Vue.js)",
+    description:
+      "Yousef Aria is a talented and innovative Front-End Developer, specializing in Vue.js, who has made significant contributions to the world of web development. With a passion for crafting seamless user experiences and a keen eye for design, Khashayar has earned a reputation for building cutting-edge and visually stunning web applications.",
+  },
+  {
+    id: "003",
+    name: "Mohammad Saba",
+    jobTitle: "Front-end Developer(React.js)",
+    description:
+      "Mohammad Saba is a talented and innovative Front-End Developer, specializing in React.js, who has made significant contributions to the world of web development. With a passion for crafting seamless user experiences and a keen eye for design, Khashayar has earned a reputation for building cutting-edge and visually stunning web applications.",
+  },
+  {
+    id: "004",
+    name: "Ali Mohammadi",
+    jobTitle: "UI/UX Designer",
+    description:
+      "Ali Mohammadi is a talented and innovative UI/UX Designer, specializing in Figma, who has made significant contributions to the world of web development. With a passion for crafting seamless user experiences and a keen eye for design, Khashayar has earned a reputation for building cutting-edge and visually stunning web applications.",
+  },
+]);
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+function readMore() {
+  showModal.value = true;
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: rgb(169, 188, 140);
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: sans-serif;
+  overflow: hidden;
+}
+
+.modal {
+  position: fixed; 
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%; 
+  height: 100%; 
+  overflow: auto; 
+  background-color: rgb(0, 0, 0); 
+  background-color: rgba(0, 0, 0, 0.4); 
+}
+
+.modal-content {
+  background-color: #fefefe;
+  border-radius: 16px;
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 3rem;
+  margin: 15% auto; 
+  padding: 16px 24px;
+  border: 1px solid #888;
+  width: 80%; 
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,113 @@
+<template>
+  <main class="main">
+    <div class="card" v-for="(item, index) in items" :key="index">
+      <section class="card-container">
+        <div class="img-container">
+          <img src="../assets/khashayar.jpeg" alt="user profile pic" />
+          <h2>{{ item.name }}</h2>
+        </div>
+        <div class="description">
+          <div class="job-title">
+            <spa>Job Title:</spa>
+            <strong>{{ item.jobTitle }}</strong>
+          </div>
+          <div class="">
+            <div class="bio">
+              <p>
+                {{ item.description }}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="actions">
+          <button class="read-more-btn" @click="$emit('read-more', 1)">Read more...</button>
+        </div>
+      </section>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const props = defineProps(["userData"]);
+const emit = defineEmits(["read-more"]);
+
+const items = ref(props.userData);
+</script>
+
+<style scoped>
+.card-container {
+  width: 350px;
+  height: 525px;
+  background-color: gainsboro;
+  border-radius: 16px;
+  border: 1px solid rgb(167, 167, 167);
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.3);
+}
+
+img {
+  width: 42px;
+  border-radius: 100%;
+  border: 2px white solid;
+}
+
+.img-container {
+  background-color: rgb(157, 157, 157);
+  height: 72px;
+  display: flex;
+  align-items: center;
+  padding: 0 12px;
+  gap: 20px;
+  border-radius: 16px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+h2 {
+  font-size: 22px;
+}
+
+.job-title {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding: 20px;
+}
+
+.bio {
+  background-color: white;
+  border-radius: 8px;
+  margin: 4px 20px 20px 20px;
+  padding: 8px 16px;
+  line-height: 28px;
+  text-align: justify;
+
+  max-height: 300px; /* Set a maximum height for the description */
+  overflow: hidden; /* Hide any overflow */
+  text-overflow: ellipsis; /* Show ellipsis (...) for long text */
+}
+
+.main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 42px;
+}
+
+.read-more-btn {
+  padding: 8px 16px;
+  background-color: aquamarine;
+  border: 1px solid rgb(43, 43, 43);
+  border-radius: 8px;
+  cursor: pointer;
+}
+
+.read-more-btn:hover {
+  background-color: rgb(180, 255, 230);
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.3);
+}
+
+.actions {
+  padding: 4px 20px;
+}
+</style>
